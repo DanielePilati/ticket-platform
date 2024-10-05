3b392f07b405c7ef229beefa3bde85936f6491bc
DanielePilati <165797370+DanielePilati@users.noreply.github.com>
2024-10-05 11:51:06 +0200

    fix available button

--- src/main/java/org/web/app/java/spring/platform/ticket/controller/UserController.java
+++ src/main/java/org/web/app/java/spring/platform/ticket/controller/UserController.java
@@ -3,9 +3,9 @@ package org.web.app.java.spring.platform.ticket.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.web.app.java.spring.platform.ticket.model.User;
 import org.web.app.java.spring.platform.ticket.service.UserService;
 
@@ -17,16 +17,18 @@ public class UserController {
 	private UserService userService;
 
 	@PostMapping("/changestatus")
-	public String setNotAvailable(Model model, Authentication authentication) {
+	public String setNotAvailable(RedirectAttributes attributes, Authentication authentication) {
 
 		User user = userService.getByUsername(authentication.getName()).get();
 		if (!user.isNotAvailable()) {
 			user.setNotAvailable(true);
+		} else {
+			user.setNotAvailable(false);
 		}
 		userService.updateUser(user);
-		model.addAttribute("user", user);
+		attributes.addFlashAttribute("user", user);
 
-		return "/pages/home";
+		return "redirect:/";
 	}
 
 }
