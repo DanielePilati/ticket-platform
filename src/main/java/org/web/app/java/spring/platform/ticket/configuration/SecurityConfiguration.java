0f10f609e93f63d6529aee2a93c12b92a0072551
DanielePilati <165797370+DanielePilati@users.noreply.github.com>
2024-10-09 02:42:30 +0200

    fix show

--- src/main/java/org/web/app/java/spring/platform/ticket/configuration/SecurityConfiguration.java
+++ src/main/java/org/web/app/java/spring/platform/ticket/configuration/SecurityConfiguration.java
@@ -21,6 +21,7 @@ public class SecurityConfiguration {
                 .requestMatchers("/tickets/**").hasAnyAuthority("USER", "ADMIN")
                 .requestMatchers("/notes/**").hasAnyAuthority("USER", "ADMIN")
                 .requestMatchers("/users/edit/**").hasAnyAuthority("USER", "ADMIN")
+                .requestMatchers("/users/changestatus").hasAnyAuthority("USER","ADMIN")
                 .requestMatchers("/users/**").hasAnyAuthority("ADMIN")
                 .requestMatchers("/**").permitAll())
         .formLogin((login) -> login
