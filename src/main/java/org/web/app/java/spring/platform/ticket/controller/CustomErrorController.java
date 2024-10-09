0dfae787f8f2beb7551cec345f682ab7ec84ef72
DanielePilati <165797370+DanielePilati@users.noreply.github.com>
2024-10-09 06:49:21 +0200

    add custom error

---
+++ src/main/java/org/web/app/java/spring/platform/ticket/controller/CustomErrorController.java
@@ -0,0 +1,36 @@
+package org.web.app.java.spring.platform.ticket.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/error")
+public class CustomErrorController implements ErrorController {
+
+	@GetMapping()
+	public String handleError(Model model, HttpServletRequest request) {
+
+		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+		if (status != null) {
+			Integer statusCode = Integer.valueOf(status.toString());
+
+			if (statusCode == HttpStatus.NOT_FOUND.value()) {
+				return "/errors/404";
+			}
+
+			model.addAttribute("statuscode", Integer.valueOf(status.toString()));
+
+			return "/errors/error";
+		}
+
+		return "/errors/error";
+	}
+}
