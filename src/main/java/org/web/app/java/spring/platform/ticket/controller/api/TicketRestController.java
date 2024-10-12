2e8157eeacec14f35528e2b8711e62ddae21fe3a
DanielePilati <165797370+DanielePilati@users.noreply.github.com>
2024-10-12 15:33:41 +0200

    fix api

--- src/main/java/org/web/app/java/spring/platform/ticket/controller/api/TicketRestController.java
+++ src/main/java/org/web/app/java/spring/platform/ticket/controller/api/TicketRestController.java
@@ -9,8 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.web.app.java.spring.platform.ticket.model.State;
 import org.web.app.java.spring.platform.ticket.model.Ticket;
+import org.web.app.java.spring.platform.ticket.model.Type;
+import org.web.app.java.spring.platform.ticket.service.StateService;
 import org.web.app.java.spring.platform.ticket.service.TicketService;
+import org.web.app.java.spring.platform.ticket.service.TypeService;
 
 @RestController
 @RequestMapping("/api/tickets")
@@ -18,13 +22,17 @@ public class TicketRestController {
 
 	@Autowired
 	private TicketService ticketService;
+	@Autowired
+	private StateService stateService;
+	@Autowired
+	private TypeService typeService;
 
 	@GetMapping()
 	public ResponseEntity<List<Ticket>> index() {
 		List<Ticket> tickets = ticketService.getAll();
 
 		if (tickets.isEmpty()) {
-			return new ResponseEntity<List<Ticket>>(tickets,HttpStatus.NO_CONTENT);
+			return new ResponseEntity<List<Ticket>>(tickets, HttpStatus.NO_CONTENT);
 		}
 		return new ResponseEntity<List<Ticket>>(tickets, HttpStatus.OK);
 	}
@@ -32,23 +40,31 @@ public class TicketRestController {
 	@GetMapping("/state/{state}")
 	public ResponseEntity<List<Ticket>> showByState(@PathVariable(name = "state") String state) {
 
-		List<Ticket> tickets = ticketService.getByState(state);
-		if (tickets.isEmpty()) {
-			return new ResponseEntity<List<Ticket>>(tickets,HttpStatus.NO_CONTENT);
+		for (State stateIn : stateService.getAll()) {
+			if (stateIn.getName().equals(state)) {
+				List<Ticket> tickets = ticketService.getByState(state);
+				if (tickets.isEmpty()) {
+					return new ResponseEntity<List<Ticket>>(tickets, HttpStatus.NO_CONTENT);
+				}
+				return new ResponseEntity<List<Ticket>>(tickets, HttpStatus.OK);
+			}
 		}
-		return new ResponseEntity<List<Ticket>>(tickets, HttpStatus.OK);
-
+		return new ResponseEntity<List<Ticket>>(HttpStatus.BAD_REQUEST);
 	}
 
 	@GetMapping("/category/{type}")
 	public ResponseEntity<List<Ticket>> showByType(@PathVariable(name = "type") String type) {
 
-		List<Ticket> tickets = ticketService.getAllByType(type);
-		if (tickets.isEmpty()) {
-			return new ResponseEntity<List<Ticket>>(tickets,HttpStatus.NO_CONTENT);
+		for (Type typeIn : typeService.getAll()) {
+			if (typeIn.getName().equals(type)) {
+				List<Ticket> tickets = ticketService.getAllByType(type);
+				if (tickets.isEmpty()) {
+					return new ResponseEntity<List<Ticket>>(tickets, HttpStatus.NO_CONTENT);
+				}
+				return new ResponseEntity<List<Ticket>>(tickets, HttpStatus.OK);
+			}
 		}
-		return new ResponseEntity<List<Ticket>>(tickets, HttpStatus.OK);
-
+		return new ResponseEntity<List<Ticket>>(HttpStatus.BAD_REQUEST);
 	}
 
 }
