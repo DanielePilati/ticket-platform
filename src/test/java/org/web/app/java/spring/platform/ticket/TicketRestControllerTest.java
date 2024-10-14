8b50f98b43559bc9cbd2defdc5fd47487b572ed2
DanielePilati <165797370+DanielePilati@users.noreply.github.com>
2024-10-14 17:06:24 +0200

    fix

--- src/test/java/org/web/app/java/spring/platform/ticket/TicketRestControllerTest.java
+++ src/test/java/org/web/app/java/spring/platform/ticket/TicketRestControllerTest.java
@@ -13,35 +13,65 @@ import org.springframework.test.web.servlet.MockMvc;
 @SpringBootTest
 @AutoConfigureMockMvc
 public class TicketRestControllerTest {
-	
+
 	@Autowired
 	private MockMvc mocMvc;
-	
+
 	@Test
 	void isEmptyCategory() throws Exception {
-		mocMvc.perform(get("/api/tickets/category/{type}", "Other")).andExpect(status().isNoContent()).andDo(print());
+		try {
+			mocMvc.perform(get("/api/tickets/category/{type}", "Other")).andExpect(status().isNoContent())
+					.andDo(print());
+		} catch (Exception exception) {
+			System.out.println("Something went wrong. Exception: " + exception);
+		}
 	}
+
 	@Test
 	void notFoundCategory() throws Exception {
-		mocMvc.perform(get("/api/tickets/category/{type}", "prova")).andExpect(status().isBadRequest()).andDo(print());
+		try {
+			mocMvc.perform(get("/api/tickets/category/{type}", "prova")).andExpect(status().isBadRequest())
+					.andDo(print());
+		} catch (Exception exception) {
+			System.out.println("Something went wrong. Exception: " + exception);
+		}
 	}
+
 	@Test
 	void thereIsCategory() throws Exception {
-		mocMvc.perform(get("/api/tickets/category/{type}", "Assistance")).andExpect(status().isOk()).andDo(print());
+		try {
+			mocMvc.perform(get("/api/tickets/category/{type}", "Assistance")).andExpect(status().isOk()).andDo(print());
+		} catch (Exception exception) {
+			System.out.println("Something went wrong. Exception: " + exception);
+		}
 	}
-	
+
 	@Test
 	void isEmptyStatus() throws Exception {
-		mocMvc.perform(get("/api/tickets/state/{state}", "ToDo")).andExpect(status().isNoContent()).andDo(print());
+		try {
+			mocMvc.perform(get("/api/tickets/state/{state}", "ToDo")).andExpect(status().isNoContent()).andDo(print());
+		} catch (Exception exception) {
+			System.out.println("Something went wrong. Exception: " + exception);
+		}
 	}
+
 	@Test
 	void notFoundStatus() throws Exception {
-		mocMvc.perform(get("/api/tickets/state/{state}", "prova")).andExpect(status().isBadRequest()).andDo(print());
+		try {
+			mocMvc.perform(get("/api/tickets/state/{state}", "prova")).andExpect(status().isBadRequest())
+					.andDo(print());
+		} catch (Exception exception) {
+			System.out.println("Something went wrong. Exception: " + exception);
+		}
 	}
+
 	@Test
 	void thereIsStatus() throws Exception {
-		mocMvc.perform(get("/api/tickets/state/{state}", "Completed")).andExpect(status().isOk()).andDo(print());
+		try {
+			mocMvc.perform(get("/api/tickets/state/{state}", "Completed")).andExpect(status().isOk()).andDo(print());
+		} catch (Exception exception) {
+			System.out.println("Something went wrong. Exception: " + exception);
+		}
 	}
-	
 
 }
